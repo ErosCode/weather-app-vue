0d2657b8b394829b0e2a8b18dbe410ff6fb7eac5
ErosCode <corentin.dc.eros@gmail.com>
2021-05-21 00:11:20 +0200

    fix css

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -53,9 +53,39 @@ export default {
 }
 </script>
 
-<style>
-    h4 {
-        padding-top: 100px;
-    }
 
+<style lang="scss" scoped>
+.loading {
+  @keyframes spin {
+    to {
+      transform: rotateZ(360deg);
+    }
+  }
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  span {
+    display: block;
+    width: 60px;
+    height: 60px;
+    margin: 0 auto;
+    border: 2px solid transparent;
+    border-top-color: #142a5f;
+    border-radius: 50%;
+    animation: spin ease 1000ms infinite;
+  }
+}
+.weather {
+  transition: 500ms ease;
+  overflow: scroll;
+  width: 100%;
+  height: 100%;
+  .weather-wrap {
+    overflow: hidden;
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+}
 </style>
